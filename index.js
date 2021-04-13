f3ee8d7b8f52329119d75f523e60a12587a71f94
Fatemeh <fatemeh.marzoughi18@gmail.com>
2021-04-13 14:46:49 +0330

    linkingPagesWithExpress.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const Joi = require('joi');
 const path = require('path');
 const express = require('express');
 
 const app = express();
 app.use(express.static('pages'));
+//for accessing to the form fields
+app.use(express.urlencoded({
+    extended: true
+  }))
 
 app.get('/' , (req , res ) =>{
 
@@ -17,9 +22,37 @@ app.get('/logIn' , (req , res ) =>{
 })
 
 app.get('/signIn' , (req , res ) =>{
-
+    //the content of the page
     res.sendFile(path.join(__dirname+'/pages/signIn/signIn.html'))
-    
+
+})
+
+app.post('/signIn/submit' , (req , res ) => {
+
+    //check if the password field matches the repeat password field or not
+    if(req.body.password != req.body.rewritePassword) return res.send('passwords does not match!');
+
+    //check their validation
+    const schema = {
+        name : Joi.string().required(),
+        username : Joi.string().min(5).max(20).required(),
+        password : Joi.string().min(5).max(20).required(),
+        email : Joi.string().email().required(),
+        phone : Joi.number().required(),
+    }
+
+    const result = Joi.validate(req.body , schema);
+
+    if(result.error)
+    {
+        res.sendFile(path.join(__dirname+'/pages/signIn/signIn.html'))
+        res.status(400).send(result.error.details[0].message);
+        return;    
+    }
+
+    //check if the user has login before or not
+
+    //insert to the db
 })
 
 app.get('/logIn/forgotPass' , (req , res ) =>{
@@ -30,3 +63,7 @@ app.get('/logIn/forgotPass' , (req , res ) =>{
 
 const port = process.env.PORT || 3000;
 app.listen(port , () => console.log(`Listening on port ${port}`));
+
+
+
+
